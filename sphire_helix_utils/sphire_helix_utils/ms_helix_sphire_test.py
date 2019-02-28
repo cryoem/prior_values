4f7cabb229f098b0c3e2c8a1a922103de4356060
Luca Lusnig <lusnig@rtxr2.client.mpi-dortmund.mpg.de>
2019-02-28 15:24:36 +0100

    added some tests to 'ms_helix_sphire_test.py'

--- sphire_helix_utils/sphire_helix_utils/ms_helix_sphire_test.py
+++ sphire_helix_utils/sphire_helix_utils/ms_helix_sphire_test.py
@@ -1,56 +1,62 @@
 import ms_helix_sphire
 import unittest
+from os import system as os_system
 
 
-class Test_get_stack_dtype(unittest.TestCase):
+RELATIVE_PATH_EMAN2= "../../EMAN2DB"
+NAME = 'bdb:stack'
+INDEX_FILE_RAW = '../tests/index_raw.txt'
+PARAMS_FILE_RAW = '../tests/params_raw.txt'
+
+
+class Test_ms_helix_sphire(unittest.TestCase):
+    SKIP_TEST = False
+
+    @classmethod
+    def setUpClass(cls):
+        print "\n\tcopying EMAN2DB folder"
+        os_system("cp ../../EMAN2DB/ -r .")
+        try:
+            cls.dataDB=ms_helix_sphire.import_sphire_stack(NAME)
+        except RuntimeError:
+            cls.SKIP_TEST = True
+            print ("\n\tWARNING: CANNOT IMPORT THE DB\n\tWARNING: CANNOT IMPORT THE DB\n\tWARNING: CANNOT IMPORT THE DB")
+
     def test_get_stack_dtype(self):
-        pass
+        dtype_list = [('ptcl_source_image', '|S200'),('filament', '|S200'),('data_n', '<i8')]
+        self.assertTrue(dtype_list == ms_helix_sphire.get_stack_dtype())
+        print "test"
 
-
-
-class Test_(unittest.TestCase):
-    def test_(self):
-        pass
-
-
-
-class Test_import_sphire_stack(unittest.TestCase):
     def test_import_sphire_stack(self):
-        pass
+        if self.SKIP_TEST is False:
+            self.assertTrue(684 == len(self.dataDB))
+            self.assertTrue(self.dataDB.dtype == [('ptcl_source_image', 'S200'), ('filament', 'S200'), ('data_n', '<i8')])
 
+    def test_import_sphire_params(self):
+        data = ms_helix_sphire.import_sphire_params(PARAMS_FILE_RAW)
+        self.assertTrue(data.dtype == [('phi', '<f8'), ('theta', '<f8'), ('psi', '<f8'), ('shift_x', '<f8'), ('shift_y', '<f8'), ('err1', '<f8'),('err2', '<f8'), ('norm', '<f8'), ('source_n', '<i8')])
+        self.assertTrue(684 == len(data))
 
-
-class Test_import_sphire_params(unittest.TestCase):
-    def test_(self):
-        pass
-
-
-
-class Test_import_sphire_index(unittest.TestCase):
     def test_import_sphire_index(self):
-        pass
+        data = ms_helix_sphire.import_sphire_index(INDEX_FILE_RAW)
+        self.assertTrue(data.dtype == [('stack_idx', '<i8')])
+        self.assertTrue(684 == len(data))
 
-
-
-class Test_write_params_file(unittest.TestCase):
     def test_write_params_file(self):
         pass
 
-
-
-class Test_write_index_file(unittest.TestCase):
     def test_write_index_file(self):
         pass
 
-
-
-class Test_write_file(unittest.TestCase):
     def test_write_file(self):
         pass
 
-
-
-class Test_prepare_output(unittest.TestCase):
     def test_prepare_output(self):
         pass
+
+
+    @classmethod
+    def tearDownClass(cls):
+        print "\n\tremove EMAN2DB folder"
+        os_system("rm -r EMAN2DB/")
 
